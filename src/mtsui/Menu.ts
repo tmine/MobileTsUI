0719a8001953935edfabbaa6da3a331d7c7760bf
Adrian Bertschi <adrian.bertschi@clip.ch>
2013-12-18 14:23:06 +0100

    - slide is now optional

--- src/mtsui/Menu.ts
+++ src/mtsui/Menu.ts
@@ -18,78 +18,82 @@ module mtsui {
         private menu: HTMLElement;
         private window: Window;
         private position: String;
-
-        constructor(window: Window, content: Component, position: string) {
+        private slide: boolean;
+        
+        constructor(window: Window, content: Component, position: string, slide?: boolean) {
             this.window = window;
             this.position = position;
+            this.slide = slide;
 
             this.menu = document.createElement("div");
             this.menu.setAttribute("class", "mtsui menu page " + position);
             this.menu.appendChild(content.getDom());
             
-            this.menu.addEventListener('touchmove', function(in_e){
-                var e = <TouchEvent> in_e;
-                if(global_window.event) global_window.event.cancelBubble = true;
-                e.stopPropagation();
-            }, false);
-            
-            var _this = this;
-            var draging = false;
-            var direction = "";
-            var lastPos = 0;
-            var visible = false;
-            var width = 0;
-            window.getDom().addEventListener('touchmove', function(in_e){
-                var page = _this.window.getActualPage();
-                var style: any = page.getDom().style;
+            if(this.slide){
+                this.menu.addEventListener('touchmove', function(in_e){
+                    var e = <TouchEvent> in_e;
+                    if(global_window.event) global_window.event.cancelBubble = true;
+                    e.stopPropagation();
+                }, false);
                 
-                width = width || window.getDom().scrollWidth;
-                
-                var e = <TouchEvent> in_e;
-                if((draging && direction == position) || (position == "left" && e.pageX <= Menu.OFFSET) || (position == "right" && e.pageX >= (width-Menu.OFFSET))){
-                    if(position == "left") direction = "left";
-                    if(position == "right") direction = "right";
-                    if(!draging){
-                        draging = true;
-                        style.transition = "none";
+                var _this = this;
+                var draging = false;
+                var direction = "";
+                var lastPos = 0;
+                var visible = false;
+                var width = 0;
+                window.getDom().addEventListener('touchmove', function(in_e){
+                    var page = _this.window.getActualPage();
+                    var style: any = page.getDom().style;
+                    
+                    width = width || window.getDom().scrollWidth;
+                    
+                    var e = <TouchEvent> in_e;
+                    if((draging && direction == position) || (position == "left" && e.pageX <= Menu.OFFSET) || (position == "right" && e.pageX >= (width-Menu.OFFSET))){
+                        if(position == "left") direction = "left";
+                        if(position == "right") direction = "right";
+                        if(!draging){
+                            draging = true;
+                            style.transition = "none";
+                        }
+                        if(!visible && lastPos != 0){
+                            _this.show();
+                            visible = true;
+                        }
+                        
+                        lastPos = e.pageX;
+                        
+                        if(direction == "left") {
+                            style.webkitTransform = "translate3d("+e.pageX+"px, 0, 0)";
+                            style.transform = "translate3d("+e.pageX+"px, 0, 0)";
+                        } else if(direction == "right") {
+                            style.webkitTransform = "translate3d("+(e.pageX-width)+"px, 0, 0)";
+                            style.transform = "translate3d("+(e.pageX-width)+"px, 0, 0)";
+                        }
+                        
+                        e.preventDefault();
                     }
-                    if(!visible && lastPos != 0){
-                        _this.show();
-                        visible = true;
+                }, false);
+                
+                window.getDom().addEventListener('touchend', function(in_e){
+                    var page = _this.window.getActualPage();
+                    var style: any = page.getDom().style;
+                    
+                    if(draging && direction == "right" && lastPos >= (width/2)){
+                        _this.hide();
+                    } else if(draging && direction == "left" && lastPos <= (width/2)){
+                        _this.hide();
                     }
                     
-                    lastPos = e.pageX;
-                    
-                    if(direction == "left") {
-                        style.webkitTransform = "translate3d("+e.pageX+"px, 0, 0)";
-                        style.transform = "translate3d("+e.pageX+"px, 0, 0)";
-                    } else if(direction == "right") {
-                        style.webkitTransform = "translate3d("+(e.pageX-width)+"px, 0, 0)";
-                        style.transform = "translate3d("+(e.pageX-width)+"px, 0, 0)";
-                    }
-                    
-                    e.preventDefault();
-                }
-            }, false);
-            
-            window.getDom().addEventListener('touchend', function(in_e){
-                var page = _this.window.getActualPage();
-                var style: any = page.getDom().style;
-                
-                if(draging && direction == "right" && lastPos >= (width/2)){
-                    _this.hide();
-                } else if(draging && direction == "left" && lastPos <= (width/2)){
-                    _this.hide();
-                }
-                
-                draging = false;
-                visible = false;
-                style.transition = "";
-                style.webkitTransform = "";
-                style.transform = "";
-                lastPos = 0;
-                direction = "";
-            }, false);
+                    draging = false;
+                    visible = false;
+                    style.transition = "";
+                    style.webkitTransform = "";
+                    style.transform = "";
+                    lastPos = 0;
+                    direction = "";
+                }, false);
+            }
 		}
         
         public addTo(header: Header, icon: Component): void{
@@ -114,64 +118,66 @@ module mtsui {
             var overlay = document.createElement("div");
             overlay.setAttribute("class", "mtsui page");
             
-            var _this = this;
-            var draging = false;
-            var direction = "";
-            var lastPos = 0;
-            var visible = false;
-            var width = 0;
-            overlay.addEventListener('touchmove', function(in_e){
-                var page = _this.window.getActualPage();
-                var style: any = page.getDom().style;
-                
-                width = width || overlay.scrollWidth;
-                                
-                var e = <TouchEvent> in_e;
-                if((draging && direction == _this.position) || (_this.position == "right" && e.pageX <= Menu.OFFSET) || (_this.position == "left" && e.pageX >= (width-Menu.OFFSET))){
-                    if(_this.position == "left") direction = "left";
-                    if(_this.position == "right") direction = "right";
-                    if(!draging){
-                        draging = true;
-                        style.transition = "none";
+            if(this.slide){
+                var _this = this;
+                var draging = false;
+                var direction = "";
+                var lastPos = 0;
+                var visible = false;
+                var width = 0;
+                overlay.addEventListener('touchmove', function(in_e){
+                    var page = _this.window.getActualPage();
+                    var style: any = page.getDom().style;
+                    
+                    width = width || overlay.scrollWidth;
+                                    
+                    var e = <TouchEvent> in_e;
+                    if((draging && direction == _this.position) || (_this.position == "right" && e.pageX <= Menu.OFFSET) || (_this.position == "left" && e.pageX >= (width-Menu.OFFSET))){
+                        if(_this.position == "left") direction = "left";
+                        if(_this.position == "right") direction = "right";
+                        if(!draging){
+                            draging = true;
+                            style.transition = "none";
+                        }
+                        if(!visible && lastPos != 0){
+                            visible = true;
+                        }
+                        
+                        lastPos = e.pageX;
+                        
+                        if(direction == "left") {
+                            style.webkitTransform = "translate3d("+e.pageX+"px, 0, 0)";
+                            style.transform = "translate3d("+e.pageX+"px, 0, 0)";
+                        } else if(direction == "right") {
+                            style.webkitTransform = "translate3d("+(e.pageX-width)+"px, 0, 0)";
+                            style.transform = "translate3d("+(e.pageX-width)+"px, 0, 0)";
+                        }
+                        
                     }
-                    if(!visible && lastPos != 0){
-                        visible = true;
+                    e.preventDefault();
+                    if(global_window.event) global_window.event.cancelBubble = true;
+                    e.stopPropagation();
+                }, false);
+                
+                overlay.addEventListener('touchend', function(in_e){
+                    var page = _this.window.getActualPage();
+                    var style: any = page.getDom().style;
+                    
+                    if(draging && direction == "right" && lastPos >= (width/2)){
+                        _this.hide();
+                    } else if(draging && direction == "left" && lastPos <= (width/2)){
+                        _this.hide();
                     }
                     
-                    lastPos = e.pageX;
-                    
-                    if(direction == "left") {
-                        style.webkitTransform = "translate3d("+e.pageX+"px, 0, 0)";
-                        style.transform = "translate3d("+e.pageX+"px, 0, 0)";
-                    } else if(direction == "right") {
-                        style.webkitTransform = "translate3d("+(e.pageX-width)+"px, 0, 0)";
-                        style.transform = "translate3d("+(e.pageX-width)+"px, 0, 0)";
-                    }
-                    
-                }
-                e.preventDefault();
-                if(global_window.event) global_window.event.cancelBubble = true;
-                e.stopPropagation();
-            }, false);
-            
-            overlay.addEventListener('touchend', function(in_e){
-                var page = _this.window.getActualPage();
-                var style: any = page.getDom().style;
-                
-                if(draging && direction == "right" && lastPos >= (width/2)){
-                    _this.hide();
-                } else if(draging && direction == "left" && lastPos <= (width/2)){
-                    _this.hide();
-                }
-                
-                draging = false;
-                visible = false;
-                style.transition = "";
-                style.webkitTransform = "";
-                style.transform = "";
-                lastPos = 0;
-                direction = "";
-            }, false);
+                    draging = false;
+                    visible = false;
+                    style.transition = "";
+                    style.webkitTransform = "";
+                    style.transform = "";
+                    lastPos = 0;
+                    direction = "";
+                }, false);
+            }
             
             this.window.getDom().appendChild(overlay);
             
